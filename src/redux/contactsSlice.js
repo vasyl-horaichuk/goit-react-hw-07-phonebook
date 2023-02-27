2a950bd33b00df288461d7502aa41124194e3f8f
Vasyl Horaichuk <vg.createam@gmail.com>
2023-02-27 12:26:00 +0200

    Test

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,6 +1,10 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-const contactsInitialState = [];
+const contactsInitialState = {
+  items: [],
+  isLoading: false,
+  error: null,
+};
 
 const contactsSlice = createSlice({
   // Ім'я слайсу
@@ -9,13 +13,28 @@ const contactsSlice = createSlice({
   initialState: contactsInitialState,
   // Об'єкт редюсерів
   reducers: {
-    addContact(state, action) {
-      state.push(action.payload);
+    fetchingInProgress(state) {
+      state.isLoading = true;
     },
-    deleteContact(state, action) {
-      const index = state.findIndex(contact => contact.id === action.payload);
-      state.splice(index, 1);
+
+    fetchingSuccess(state, action) {
+      state.isLoading = false;
+      state.error = null;
+      state.action = action.payload;
     },
+
+    fetchingError(state, action) {
+      state.isLoading = false;
+      state.error = action.payload;
+    },
+
+    // addContact(state, action) {
+    //   state.push(action.payload);
+    // },
+    // deleteContact(state, action) {
+    //   const index = state.findIndex(contact => contact.id === action.payload);
+    //   state.splice(index, 1);
+    // },
   },
 });
 
--- src/redux/operation.js
+++ src/redux/operation.js
@@ -1,1 +1,22 @@
-export const fetch = () => {};
+import axios from 'axios';
+import {
+  fetchingInProgress,
+  fetchingSuccess,
+  fetchingError,
+} from './contactsSlice';
+
+axios.defaults.baseURL = 'https://63fb9feb6deb8bdb81488c5c.mockapi.io';
+
+export const fetchContacts = () => async dispatch => {
+  try {
+    // Індикатор завантаження
+    dispatch(fetchingInProgress());
+    // HTTP-запит
+    const response = await axios.get('/contacts');
+    // Обробка даних
+    dispatch(fetchingSuccess(response.data));
+    // Обробка помилки
+  } catch (e) {
+    dispatch(fetchingError(e.message));
+  }
+};
