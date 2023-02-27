108dee5d33c8680e01a57b5640de5316f672f225
Vasyl Horaichuk <vg.createam@gmail.com>
2023-02-27 17:45:07 +0200

    update

--- src/redux/operation.js
+++ src/redux/operation.js
@@ -1,23 +1,12 @@
 import axios from 'axios';
-import {
-  fetchingInProgress,
-  fetchingSuccess,
-  fetchingError,
-} from './contactsSlice';
+import { createAsyncThunk } from '@reduxjs/toolkit';
 
 axios.defaults.baseURL = 'https://63fb9feb6deb8bdb81488c5c.mockapi.io';
 
-export const fetchContacts = () => async dispatch => {
-  try {
-    // Індикатор завантаження
-    dispatch(fetchingInProgress());
-    // HTTP-запит
-    const response = await axios.get('contacts');
-    // Обробка даних
-    dispatch(fetchingSuccess(response.data));
-    console.log(response.data);
-    // Обробка помилки
-  } catch (e) {
-    dispatch(fetchingError(e.message));
+export const fetchContacts = createAsyncThunk(
+  '/contacts/fetchAll',
+  async () => {
+    const response = await axios.get('/contacts');
+    return response.data;
   }
-};
+);
